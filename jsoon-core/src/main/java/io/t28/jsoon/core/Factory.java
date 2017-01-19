5173ee592793d5aa4133e1495f0817af8c8ad1dd
Tatsuya Maki <t28oogle@gmail.com>
2017-01-20 00:32:13 +0900

    Added example implementation to generate Java file from JSON (#2)

---
+++ jsoon-core/src/main/java/io/t28/jsoon/core/Factory.java
@@ -0,0 +1,96 @@
+package io.t28.jsoon.core;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Files;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.annotation.Nonnull;
+import javax.lang.model.element.Modifier;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class Factory {
+    private final ObjectMapper mapper;
+
+    public Factory(@Nonnull ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Nonnull
+    public String create(@Nonnull String packageName, @Nonnull String className, @Nonnull String json) throws Exception {
+        final JsonNode rootNode = mapper.readTree(json);
+        if (!rootNode.isObject() && !rootNode.isArray()) {
+            throw new IllegalArgumentException();
+        }
+
+        final TypeSpec typeSpec = create(className, rootNode)
+                .toBuilder()
+                .addAnnotation(AnnotationSpec.builder(SuppressWarnings.class)
+                        .addMember("value", "$S", "all")
+                        .build())
+                .build();
+        final JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
+                .indent("    ")
+                .skipJavaLangImports(true)
+                .build();
+        return javaFile.toString();
+    }
+
+    @Nonnull
+    private TypeSpec create(String name, JsonNode node) {
+        final TypeSpec.Builder classBuilder = TypeSpec.classBuilder(name)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .addAnnotation(AnnotationSpec.builder(JsonIgnoreProperties.class)
+                        .addMember("ignoreUnknown", "$L", true)
+                        .build());
+        final MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(AnnotationSpec.builder(JsonCreator.class)
+                        .build());
+        final Iterable<Map.Entry<String, JsonNode>> fields = node::fields;
+        fields.forEach(field -> {
+            final String childName = field.getKey();
+            final JsonNode childNode = field.getValue();
+
+            if (childNode.isTextual()) {
+                constructorBuilder.addParameter(ParameterSpec.builder(String.class, childName)
+                        .addAnnotation(AnnotationSpec.builder(JsonProperty.class)
+                                .addMember("value", "$S", childName)
+                                .build())
+                        .build())
+                        .addStatement("this.$1L = $1L", childName);
+                classBuilder.addField(FieldSpec.builder(String.class, childName)
+                        .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
+                        .build());
+                classBuilder.addMethod(MethodSpec.methodBuilder(childName)
+                        .returns(String.class)
+                        .addModifiers(Modifier.PUBLIC)
+                        .addAnnotation(AnnotationSpec.builder(JsonProperty.class)
+                                .addMember("value", "$S", childName)
+                                .build())
+                        .addStatement("return $L", childName)
+                        .build());
+            }
+        });
+        classBuilder.addMethod(constructorBuilder.build());
+        return classBuilder.build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        final File file = new File("jsoon-core/src/main/resources/user.json");
+        final String json = Files.toString(file, StandardCharsets.UTF_8);
+        final Factory factory = new Factory(new ObjectMapper());
+        final String javaFile = factory.create("io.t28.jsoon.example", "Example", json);
+        System.out.println(javaFile);
+    }
+}
