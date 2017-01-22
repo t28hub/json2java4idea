ded8fca818f5684d799dbcd588f20aa5225d71a6
Tatsuya Maki <t28oogle@gmail.com>
2017-01-22 19:44:01 +0900

    Added CaseType which represents naming case type (#10)

---
+++ core/src/main/java/io/t28/jsoon/core/naming/CaseType.java
@@ -0,0 +1,37 @@
+package io.t28.jsoon.core.naming;
+
+import com.google.common.base.CaseFormat;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
+public enum CaseType {
+    LOWER_CAMEL_CASE(CaseFormat.LOWER_CAMEL),
+    UPPER_CAMEL_CASE(CaseFormat.UPPER_CAMEL),
+    LOWER_SNAKE_CASE(CaseFormat.LOWER_UNDERSCORE),
+    UPPER_SNAKE_CASE(CaseFormat.UPPER_UNDERSCORE),
+    LOWER_KEBAB_CASE(CaseFormat.LOWER_HYPHEN),
+    UPPER_KEBAB_CASE(CaseFormat.LOWER_HYPHEN) {
+        @Nonnull
+        @Override
+        public String to(@Nonnull CaseType type, @Nonnull String text) {
+            return super.to(type, text.toLowerCase());
+        }
+    };
+
+    private final CaseFormat format;
+
+    CaseType(@Nonnull CaseFormat format) {
+        this.format = format;
+    }
+
+    @Nonnull
+    @CheckReturnValue
+    public String to(@Nonnull CaseType type, @Nonnull String text) {
+        final String converted = this.format.to(type.format, text);
+        if (type == UPPER_KEBAB_CASE) {
+            return converted.toUpperCase();
+        }
+        return converted;
+    }
+}
