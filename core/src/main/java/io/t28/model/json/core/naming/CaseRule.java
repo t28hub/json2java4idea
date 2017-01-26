191da5783f97ce599c42f2e5fb2547300e4890fc
Tatsuya Maki <t28oogle@gmail.com>
2017-01-26 18:05:58 +0900

    Added CaseRule enum (#19)

---
+++ core/src/main/java/io/t28/model/json/core/naming/CaseRule.java
@@ -0,0 +1,37 @@
+package io.t28.model.json.core.naming;
+
+import com.google.common.base.CaseFormat;
+
+import javax.annotation.Nonnull;
+
+public enum CaseRule {
+    LOWER_CAMEL_CASE(CaseFormat.LOWER_CAMEL),
+    UPPER_CAMEL_CASE(CaseFormat.UPPER_CAMEL),
+    LOWER_SNAKE_CASE(CaseFormat.LOWER_UNDERSCORE),
+    UPPER_SNAKE_CASE(CaseFormat.UPPER_UNDERSCORE),
+    LOWER_KEBAB_CASE(CaseFormat.LOWER_HYPHEN),
+    UPPER_KEBAB_CASE(CaseFormat.LOWER_HYPHEN) {
+        @Nonnull
+        @Override
+        public String to(@Nonnull CaseRule rule, @Nonnull String text) {
+            return super.to(rule, text.toLowerCase());
+        }
+    };
+
+    private final CaseFormat format;
+
+    CaseRule(@Nonnull CaseFormat format) {
+        this.format = format;
+    }
+
+    @Nonnull
+    public String to(@Nonnull CaseRule rule, @Nonnull String text) {
+        if (this == rule) {
+            return text;
+        }
+        if (rule == CaseRule.UPPER_KEBAB_CASE) {
+            return this.format.to(rule.format, text).toUpperCase();
+        }
+        return this.format.to(rule.format, text);
+    }
+}
