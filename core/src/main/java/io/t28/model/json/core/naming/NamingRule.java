45beaab192b7d8b00986b25e82247406ae25cdd2
Tatsuya Maki <t28oogle@gmail.com>
2017-01-28 15:35:38 +0900

    Added Metadata and Metadata.Property classes (#21)

--- core/src/main/java/io/t28/model/json/core/naming/NamingRule.java
+++ core/src/main/java/io/t28/model/json/core/naming/NamingRule.java
@@ -22,6 +22,15 @@ public interface NamingRule {
     Optional<String> suffix();
 
     @Nonnull
+    default String format(@Nonnull CaseRule rule, @Nonnull String name) {
+        final StringBuilder builder = new StringBuilder(name.length());
+        builder.append(rule.to(CaseRule.UPPER_KEBAB_CASE, name));
+        prefix().ifPresent(prefix -> builder.append(prefix).append("-"));
+        suffix().ifPresent(suffix -> builder.append("-").append(suffix));
+        return CaseRule.UPPER_KEBAB_CASE.to(rule(), builder.toString());
+    }
+
+    @Nonnull
     @CheckReturnValue
     static ImmutableNamingRule.Builder builder() {
         return ImmutableNamingRule.builder();
