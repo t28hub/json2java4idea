82ad96ca299933cf7539dee545890d8301f1e64a
Tatsuya Maki <t28oogle@gmail.com>
2017-01-28 18:48:32 +0900

    Added ClassFactory which creates TypeSpec from JSON

--- core/src/main/java/io/t28/model/json/core/ModelJson.java
+++ core/src/main/java/io/t28/model/json/core/ModelJson.java
@@ -1,17 +1,14 @@
 package io.t28.model.json.core;
 
 import com.google.common.io.Files;
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import io.t28.model.json.core.builder.ClassBuilder;
-import io.t28.model.json.core.builder.ModelClassBuilder;
-import io.t28.model.json.core.json.JsonObject;
 import io.t28.model.json.core.json.JsonValue;
+import io.t28.model.json.core.naming.NamingCase;
+import io.t28.model.json.core.naming.NamingRule;
 import io.t28.model.json.core.parser.JacksonParser;
 import io.t28.model.json.core.parser.JsonParser;
 
-import javax.lang.model.element.Modifier;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 
@@ -19,39 +16,25 @@ public class ModelJson {
     public static void main(String[] args) throws Exception {
         final JsonParser parser = new JacksonParser();
 
-        final File userFile = new File("core/src/main/resources/user.json");
-        final String userJson = Files.toString(userFile, StandardCharsets.UTF_8);
-        final JsonValue user = parser.parse(userJson);
-        if (!user.isObject()) {
-            throw new RuntimeException();
-        }
+        final File file = new File("core/src/main/resources/repositories.json");
+        final String json = Files.toString(file, StandardCharsets.UTF_8);
+        final JsonValue value = parser.parse(json);
 
-        final TypeSpec generated = generate(user.asObject(), "User", Modifier.PUBLIC);
-        System.out.println(generated);
-    }
-
-    private static TypeSpec generate(JsonObject object, String className, Modifier... modifiers) {
-        final ClassBuilder builder = new ModelClassBuilder(className);
-        builder.addModifiers(modifiers);
-        object.stream().forEach(child -> {
-            final String name = child.getKey();
-            final JsonValue value = child.getValue();
-            if (value.isObject()) {
-                final TypeSpec enclosedClass = generate(value.asObject(), name, Modifier.PUBLIC, Modifier.STATIC);
-                builder.addEnclosedClass(enclosedClass);
-
-                final TypeName type = ClassName.bestGuess(enclosedClass.name);
-                builder.addProperty(name, type);
-                return;
-            }
-
-            if (value.isArray()) {
-
-                return;
-            }
-
-            builder.addProperty(name, value.getType());
-        });
-        return builder.build();
+        final ClassFactory factory = new ClassFactory(
+                ClassBuilder.Type.MODEL,
+                NamingCase.LOWER_SNAKE_CASE,
+                NamingRule.builder()
+                        .namingCase(NamingCase.UPPER_CAMEL_CASE)
+                        .build(),
+                NamingRule.builder()
+                        .namingCase(NamingCase.LOWER_CAMEL_CASE)
+                        .prefix("m")
+                        .build(),
+                NamingRule.builder()
+                        .namingCase(NamingCase.LOWER_CAMEL_CASE)
+                        .build()
+        );
+        final TypeSpec created = factory.create("User", value);
+        System.out.println(created.toString());
     }
 }
