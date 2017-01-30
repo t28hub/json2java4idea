25efd1ccfe5b7c6ec1b0eb6d5ad210796bbe5c0c
Tatsuya Maki <t28oogle@gmail.com>
2017-01-31 00:00:27 +0900

    Refactored ModelClassBuilder (#31)

--- core/src/main/java/io/t28/model/json/core/builder/ModelClassBuilder.java
+++ core/src/main/java/io/t28/model/json/core/builder/ModelClassBuilder.java
@@ -1,5 +1,6 @@
 package io.t28.model.json.core.builder;
 
+import com.google.common.collect.ImmutableList;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
@@ -10,6 +11,8 @@ import io.t28.model.json.core.naming.NamingStrategy;
 
 import javax.annotation.Nonnull;
 import javax.lang.model.element.Modifier;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class ModelClassBuilder extends ClassBuilder {
     ModelClassBuilder(@Nonnull String name, @Nonnull Context context) {
@@ -21,37 +24,57 @@ class ModelClassBuilder extends ClassBuilder {
     public TypeSpec build() {
         final TypeSpec.Builder classBuilder = TypeSpec.classBuilder(getName());
         getModifiers().forEach(classBuilder::addModifiers);
+        classBuilder.addFields(buildFields());
+        classBuilder.addMethods(buildMethods());
+        classBuilder.addTypes(getInnerClasses());
+        return classBuilder.build();
+    }
 
-        final MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder();
-        constructorBuilder.addModifiers(Modifier.PUBLIC);
+    @Nonnull
+    private List<FieldSpec> buildFields() {
+        final Context context = getContext();
+        final NamingStrategy fieldNameStrategy = context.fieldNameStrategy();
+        return getProperties()
+                .entrySet()
+                .stream()
+                .map(property -> {
+                    final String name = property.getKey();
+                    final TypeName type = property.getValue();
+                    final String fieldName = fieldNameStrategy.transform(name, type);
+                    return FieldSpec.builder(type, fieldName)
+                            .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Nonnull
+    private List<MethodSpec> buildMethods() {
+        final MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC);
 
         final Context context = getContext();
         final NamingStrategy fieldNameStrategy = context.fieldNameStrategy();
         final NamingStrategy methodNameStrategy = context.methodNameStrategy();
+        final NamingStrategy propertyNameStrategy = context.propertyNameStrategy();
+        final ImmutableList.Builder<MethodSpec> builder = ImmutableList.builder();
         getProperties().entrySet().forEach(property -> {
             final String name = property.getKey();
             final TypeName type = property.getValue();
-
             final String fieldName = fieldNameStrategy.transform(name, type);
-            classBuilder.addField(FieldSpec.builder(type, fieldName)
-                    .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
-                    .build());
-
             final String methodName = methodNameStrategy.transform(name, type);
-            classBuilder.addMethod(MethodSpec.methodBuilder(methodName)
+            builder.add(MethodSpec.methodBuilder(methodName)
                     .addModifiers(Modifier.PUBLIC)
                     .returns(type)
                     .addStatement("return $L", fieldName)
                     .build());
 
-            final String propertyName = context.propertyNameStrategy().transform(name, type);
+            final String propertyName = propertyNameStrategy.transform(name, type);
             constructorBuilder.addParameter(ParameterSpec.builder(type, propertyName)
                     .build())
                     .addStatement("this.$L = $L", fieldName, propertyName);
-
         });
-        classBuilder.addMethod(constructorBuilder.build());
-        classBuilder.addTypes(getInnerClasses());
-        return classBuilder.build();
+        builder.add(constructorBuilder.build());
+        return builder.build();
     }
 }
