05630bb1f6f84e790f0465b8f9ff6e904b59a666
Tatsuya Maki <t28oogle@gmail.com>
2017-02-03 20:03:45 +0900

    Implemented update method to toggle visibility (#38)

--- idea/src/GenerateAction.java
+++ idea/src/GenerateAction.java
@@ -1,10 +1,45 @@
+import com.intellij.ide.IdeView;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.util.PlatformIcons;
+import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes;
 
 public class GenerateAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent event) {
         System.out.println(event);
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+        final IdeView view = event.getData(LangDataKeys.IDE_VIEW);
+        if (view == null) {
+            event.getPresentation().setEnabledAndVisible(false);
+            return;
+        }
+
+        final Project project = event.getData(LangDataKeys.PROJECT);
+        if (project == null) {
+            event.getPresentation().setEnabledAndVisible(false);
+            return;
+        }
+
+        final ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+        final PsiDirectory[] directories = view.getDirectories();
+        for (final PsiDirectory directory : directories) {
+            final boolean isSourceRoot = fileIndex.isUnderSourceRootOfType(directory.getVirtualFile(), JavaModuleSourceRootTypes.SOURCES);
+            if (isSourceRoot) {
+                event.getPresentation().setIcon(PlatformIcons.CLASS_ICON);
+                event.getPresentation().setEnabledAndVisible(true);
+                return;
+            }
+        }
+        event.getPresentation().setEnabledAndVisible(false);
     }
 }
