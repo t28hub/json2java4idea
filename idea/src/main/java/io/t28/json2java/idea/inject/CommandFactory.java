3f07e1245a723d3f365eec899dfb85faea8455f1
Tatsuya Maki <t28oogle@gmail.com>
2017-03-04 23:31:28 +0900

    Modified way to refresh document in a View (#87)

--- idea/src/main/java/io/t28/json2java/idea/inject/CommandFactory.java
+++ idea/src/main/java/io/t28/json2java/idea/inject/CommandFactory.java
@@ -1,25 +1,14 @@
 package io.t28.json2java.idea.inject;
 
 import com.google.inject.assistedinject.Assisted;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiDirectory;
 import io.t28.json2java.core.JavaConverter;
-import io.t28.json2java.idea.commands.FormatJsonCommand;
 import io.t28.json2java.idea.commands.NewClassCommand;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
 public interface CommandFactory {
-    @Nonnull
-    @CheckReturnValue
-    FormatJsonCommand create(
-            @Nonnull String json,
-            @Nonnull Editor editor,
-            @Nonnull Document document
-    );
-
     @Nonnull
     @CheckReturnValue
     NewClassCommand create(
--- idea/src/main/java/io/t28/json2java/idea/utils/Formatter.java
+++ idea/src/main/java/io/t28/json2java/idea/utils/Formatter.java
@@ -1,6 +1,5 @@
 package io.t28.json2java.idea.utils;
 
-import com.google.inject.Inject;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
@@ -14,17 +13,18 @@ public class Formatter {
     private static final String NAME = "temporary";
 
     private final PsiFileFactory fileFactory;
+    private final FileType fileType;
 
-    @Inject
-    public Formatter(@Nonnull PsiFileFactory fileFactory) {
+    public Formatter(@Nonnull PsiFileFactory fileFactory, @Nonnull FileType fileType) {
         this.fileFactory = fileFactory;
+        this.fileType = fileType;
     }
 
     @NotNull
     @CheckReturnValue
-    public String format(@Nonnull String text, @Nonnull FileType type) {
-        final String name = Extensions.append(NAME, type);
-        final PsiFile file = fileFactory.createFileFromText(name, type, text);
+    public String format(@Nonnull String text) {
+        final String name = Extensions.append(NAME, fileType);
+        final PsiFile file = fileFactory.createFileFromText(name, fileType, text);
         final CodeStyleManager styleManager = CodeStyleManager.getInstance(file.getProject());
         styleManager.reformat(file);
         return file.getText();
