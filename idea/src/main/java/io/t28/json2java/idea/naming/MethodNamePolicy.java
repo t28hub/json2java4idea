cb4f1c17ca5aa04bb99b04f4f1dff316604101a0
Tatsuya Maki <t28oogle@gmail.com>
2017-03-13 22:32:54 +0900

    Added unit tests for the MethodNamePolicy

--- idea/src/main/java/io/t28/json2java/idea/naming/MethodNamePolicy.java
+++ idea/src/main/java/io/t28/json2java/idea/naming/MethodNamePolicy.java
@@ -2,10 +2,10 @@ package io.t28.json2java.idea.naming;
 
 import com.google.common.base.CaseFormat;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.intellij.codeInsight.generation.GenerateMembersUtil;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiNameHelper;
 import com.intellij.psi.PsiType;
 import com.squareup.javapoet.TypeName;
 import io.t28.json2java.core.naming.DefaultNamePolicy;
@@ -16,15 +16,11 @@ import javax.annotation.Nonnull;
 
 public class MethodNamePolicy implements NamePolicy {
     private final Project project;
-    private final PsiNameHelper nameHelper;
     private final PsiTypeConverter typeConverter;
 
     @Inject
-    public MethodNamePolicy(@Nonnull Project project,
-                            @Nonnull PsiNameHelper nameHelper,
-                            @Nonnull PsiTypeConverter typeConverter) {
+    public MethodNamePolicy(@Nonnull Project project, @Nonnull PsiTypeConverter typeConverter) {
         this.project = Preconditions.checkNotNull(project);
-        this.nameHelper = Preconditions.checkNotNull(nameHelper);
         this.typeConverter = Preconditions.checkNotNull(typeConverter);
     }
 
@@ -32,11 +28,11 @@ public class MethodNamePolicy implements NamePolicy {
     @Override
     public String convert(@Nonnull String name, @Nonnull TypeName type) {
         final String variableName = DefaultNamePolicy.format(name, CaseFormat.LOWER_CAMEL);
-        final PsiType psiType = typeConverter.apply(type);
-        final String methodName = GenerateMembersUtil.suggestGetterName(variableName, psiType, project);
-        if (nameHelper.isKeyword(methodName)) {
-            throw new IllegalArgumentException("Generated method name is reserved by Java");
+        if (Strings.isNullOrEmpty(variableName)) {
+            throw new IllegalArgumentException("Cannot convert '" + name + "' to a method name");
         }
-        return methodName;
+
+        final PsiType psiType = typeConverter.apply(type);
+        return GenerateMembersUtil.suggestGetterName(variableName, psiType, project);
     }
 }
--- idea/src/test/java/io/t28/json2java/idea/IdeaProjectTest.java
+++ idea/src/test/java/io/t28/json2java/idea/IdeaProjectTest.java
@@ -1,11 +1,15 @@
 package io.t28.json2java.idea;
 
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
 import com.intellij.testFramework.fixtures.JavaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import org.assertj.core.util.CheckReturnValue;
+import org.assertj.swing.dependency.jsr305.Nonnull;
 import org.junit.After;
 import org.junit.Before;
 
@@ -26,9 +30,20 @@ public class IdeaProjectTest {
         fixture.tearDown();
     }
 
+    @Nonnull
+    @CheckReturnValue
+    protected Application getApplication() {
+        if (fixture == null) {
+            throw new AssertionError("getApplication() must call after setUp()");
+        }
+        return ApplicationManager.getApplication();
+    }
+
+    @Nonnull
+    @CheckReturnValue
     protected Project getProject() {
         if (fixture == null) {
-            throw new AssertionError("fixture == null");
+            throw new AssertionError("getProject() must call after setUp()");
         }
         return fixture.getProject();
     }
