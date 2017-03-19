3d3768ac14eff75a9967b44a88df754944777c76
Tatsuya Maki <t28oogle@gmail.com>
2017-03-19 17:13:58 +0900

    Fixed a method name generation bug

--- idea/src/main/java/io/t28/json2java/idea/naming/MethodNamePolicy.java
+++ idea/src/main/java/io/t28/json2java/idea/naming/MethodNamePolicy.java
@@ -23,6 +23,7 @@ import com.google.inject.Inject;
 import com.intellij.codeInsight.generation.GenerateMembersUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiType;
+import com.intellij.util.IncorrectOperationException;
 import com.squareup.javapoet.TypeName;
 import io.t28.json2java.core.naming.DefaultNamePolicy;
 import io.t28.json2java.core.naming.NamePolicy;
@@ -43,12 +44,16 @@ public class MethodNamePolicy implements NamePolicy {
     @Nonnull
     @Override
     public String convert(@Nonnull String name, @Nonnull TypeName type) {
-        final String variableName = DefaultNamePolicy.format(name, CaseFormat.LOWER_CAMEL);
+        final String variableName = DefaultNamePolicy.format(name, CaseFormat.UPPER_CAMEL);
         if (Strings.isNullOrEmpty(variableName)) {
             throw new IllegalArgumentException("Cannot convert '" + name + "' to a method name");
         }
 
-        final PsiType psiType = typeConverter.apply(type);
-        return GenerateMembersUtil.suggestGetterName(variableName, psiType, project);
+        try {
+            final PsiType psiType = typeConverter.apply(type);
+            return GenerateMembersUtil.suggestGetterName(variableName, psiType, project);
+        } catch (IncorrectOperationException e) {
+            throw new IllegalArgumentException("Cannot convert '" + name + "' to a method name", e);
+        }
     }
 }
